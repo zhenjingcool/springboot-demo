21e93b36b73a42c94568ba64c5335dcda79ca011
zhenjingcool <824742327@qq.com>
2019-02-24 16:27:03 +0800

    add file

--- springboot/src/main/java/szj/springboot/TestController.java
+++ springboot/src/main/java/szj/springboot/TestController.java
@@ -1,19 +1,26 @@
 package szj.springboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by Administrator on 2019/2/13.
  */
+@CrossOrigin
 @Controller
 public class TestController {
 
     @ResponseBody
     @RequestMapping("/hello")
-    public String hello()
+    public String hello(HttpServletRequest request)
     {
-        return "hello woild";
+        //跨域调用时会有问题，解决跨域问题
+        String callback = request.getParameter("callback");
+        String json = "{ \"firstName\":\"John\" , \"lastName\":\"Doe\" }";
+        return callback + "(" + json + ")";
     }
 }
